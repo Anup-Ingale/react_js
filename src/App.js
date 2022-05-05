67f5d4063c0f63a108dce6fa5c6f52051d8b7344
Anup-Ingale <anuingale34@gmail.com>
2022-05-05 12:37:22 +0530

    initialization of project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Anup Ingale
         </a>
       </header>
     </div>
